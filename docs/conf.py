dee647f41da4f79b61ea65f42bf8333299e9b371
BlueHouseLab <devel@bluehouselab.com>
2014-08-03 04:43:03 +0900

    fix conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -100,10 +100,12 @@ pygments_style = 'sphinx'
 # a list of builtin themes.
 html_theme = 'default'
 
+'''
 if os.environ.get('READTHEDOCS', 'False') == 'True':
     import sphinx_rtd_theme
     html_theme = "sphinx_rtd_theme"
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+'''
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the

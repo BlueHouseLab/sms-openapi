23fe47c80f0c11882479543101bcaa118e28aa35
BlueHouseLab <devel@bluehouselab.com>
2014-08-02 16:26:02 +0900

    add node.js example

---
+++ node.js/result.js
@@ -0,0 +1,33 @@
+var c = require('./conf');
+var https = require("https");
+var credential = 'Basic '+new Buffer(c.APPID+':'+c.APIKEY).toString('base64');
+if(process.argv.length != 3) {
+	console.log('Usage: ');
+	console.log(' node result.js delivery-id');
+	process.exit(1);
+}
+var deliveryid = process.argv[2];
+
+var options = {
+  host: 'api.bluehouselab.com',
+  port: 443,
+  path: '/smscenter/v1.0/result/'+deliveryid,
+  headers: {
+    'Authorization': credential,
+  },
+  method: 'GET'
+};
+var req = https.request(options, function(res) {
+  console.log(res.statusCode);
+  var body = "";
+  res.on('data', function(d) {
+    body += d;
+  });
+  res.on('end', function(d) {
+    console.log(JSON.parse(body));
+  });
+});
+req.end();
+req.on('error', function(e) {
+	console.error(e);
+});
---
+++ node.js/sendsms.js
@@ -0,0 +1,38 @@
+var c = require('./conf');
+var https = require("https");
+var credential = 'Basic '+new Buffer(c.APPID+':'+c.APIKEY).toString('base64');
+
+
+var data = {
+  "sender"     : c.SENDER,
+  "receivers"  : c.RECEIVERS,
+  "content"    : c.CONTENT
+}
+var body = JSON.stringify(data);
+
+var options = {
+  host: 'api.bluehouselab.com',
+  port: 443,
+  path: '/smscenter/v1.0/sendsms',
+  headers: {
+    'Authorization': credential,
+    'Content-Type': 'application/json; charset=utf-8',
+    'Content-Length': Buffer.byteLength(body)
+  },
+  method: 'POST'
+};
+var req = https.request(options, function(res) {
+  console.log(res.statusCode);
+  var body = "";
+  res.on('data', function(d) {
+    body += d;
+  });
+  res.on('end', function(d) {
+    console.log(JSON.parse(body));
+  });
+});
+req.write(body);
+req.end();
+req.on('error', function(e) {
+	console.error(e);
+});

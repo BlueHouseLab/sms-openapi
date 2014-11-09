6ec3c4326da148682e85ad79f0b7ba799e5389f2
BlueHouseLab <devel@bluehouselab.com>
2014-11-09 14:09:51 +0900

    Add AuthCache for efficient HTTP Reqeust (no 401)

--- java/Result.java
+++ java/Result.java
@@ -2,14 +2,19 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import org.apache.http.Header;
 import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.BasicAuthCache;
+import org.apache.http.impl.auth.BasicScheme;
+import org.apache.http.client.AuthCache;
+import org.apache.http.HttpHost;
+import org.apache.http.client.protocol.HttpClientContext;
 
 
 public final class Result {
@@ -19,17 +24,30 @@ public final class Result {
             System.err.println("  ./result delivery-id");
 			return;
 		}
-        String url = "https://api.bluehouselab.com/smscenter/v1.0/result/"+args[0];
+        String hostname = "api.bluehouselab.com";
+        String url = "https://"+hostname+"/smscenter/v1.0/result/"+args[0];
+
+		CredentialsProvider credsProvider = new BasicCredentialsProvider();
+		credsProvider.setCredentials(
+			new AuthScope(hostname, 443, AuthScope.ANY_REALM),
+			new UsernamePasswordCredentials(Config.appid, Config.apikey)
+		);
+
+		// Create AuthCache instance
+		AuthCache authCache = new BasicAuthCache();
+		authCache.put(new HttpHost(hostname, 443, "https"), new BasicScheme());
+
+		// Add AuthCache to the execution context
+		HttpClientContext context = HttpClientContext.create();
+		context.setCredentialsProvider(credsProvider);
+		context.setAuthCache(authCache);
+
         DefaultHttpClient client = new DefaultHttpClient();
+
         try {
-            // basic authentication
-            client.getCredentialsProvider().setCredentials(
-                AuthScope.ANY,
-                new UsernamePasswordCredentials(Config.appid, Config.apikey)
-            );
             HttpGet httpGet = new HttpGet(url);
 
-            HttpResponse httpResponse = client.execute(httpGet);
+            HttpResponse httpResponse = client.execute(httpGet, context);
             System.out.println(httpResponse.getStatusLine().getStatusCode());
 
             InputStream inputStream = httpResponse.getEntity().getContent();
--- java/SendSMS.java
+++ java/SendSMS.java
@@ -2,14 +2,21 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import org.apache.http.Header;
 import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.BasicAuthCache;
+import org.apache.http.impl.auth.BasicScheme;
+import org.apache.http.client.AuthCache;
+import org.apache.http.HttpHost;
+import org.apache.http.client.protocol.HttpClientContext;
+
+
 
 /* XXX didn't use org.json to be simple
 import org.json.JSONObject; 
@@ -17,14 +24,27 @@ import org.json.JSONObject;
 
 public final class SendSMS {
     public static void main(String[] args) {
-        String url = "https://api.bluehouselab.com/smscenter/v1.0/sendsms";
+        String hostname = "api.bluehouselab.com";
+        String url = "https://"+hostname+"/smscenter/v1.0/sendsms";
+
+		CredentialsProvider credsProvider = new BasicCredentialsProvider();
+		credsProvider.setCredentials(
+			new AuthScope(hostname, 443, AuthScope.ANY_REALM),
+			new UsernamePasswordCredentials(Config.appid, Config.apikey)
+		);
+
+		// Create AuthCache instance
+		AuthCache authCache = new BasicAuthCache();
+		authCache.put(new HttpHost(hostname, 443, "https"), new BasicScheme());
+
+		// Add AuthCache to the execution context
+		HttpClientContext context = HttpClientContext.create();
+		context.setCredentialsProvider(credsProvider);
+		context.setAuthCache(authCache);
+
         DefaultHttpClient client = new DefaultHttpClient();
+
         try {
-            // basic authentication
-            client.getCredentialsProvider().setCredentials(
-                AuthScope.ANY,
-                new UsernamePasswordCredentials(Config.appid, Config.apikey)
-            );
             HttpPost httpPost = new HttpPost(url);
             httpPost.setHeader("Content-type", "application/json; charset=utf-8");
             String json = "{\"sender\":\""+Config.sender+"\",\"receivers\":[\""+Config.receiver+"\"],\"content\":\""+Config.content+"\"}";
@@ -32,7 +52,7 @@ public final class SendSMS {
             StringEntity se = new StringEntity(json, "UTF-8");
             httpPost.setEntity(se);
 
-            HttpResponse httpResponse = client.execute(httpPost);
+            HttpResponse httpResponse = client.execute(httpPost, context);
             System.out.println(httpResponse.getStatusLine().getStatusCode());
 
             InputStream inputStream = httpResponse.getEntity().getContent();
